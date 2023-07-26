19e11fd345af1d2a77610c9e139e6c8466996fa3
andreeacioca1701 <135630713+andreeacioca1701@users.noreply.github.com>
2023-07-26 12:21:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+const form = document.querySelector('.form');
